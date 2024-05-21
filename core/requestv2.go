aed41f1227e0b6f8bc5ba59c38863e05f2df9016
cw-kajiwara <kajiwara@chatwork.com>
2024-05-21 14:34:28 +0900

    print request in more detail

--- core/requestv2.go
+++ core/requestv2.go
@@ -114,7 +114,7 @@ func (r *RequestAccessorV2) EventToRequestWithContext(ctx context.Context, req e
 // Returns the populated request maintaining headers
 func (r *RequestAccessorV2) EventToRequest(req events.APIGatewayV2HTTPRequest) (*http.Request, error) {
 	// debug print req
-	fmt.Println("Request: ", req.RequestContext.HTTP.Method, req.RequestContext.HTTP.Path)
+	fmt.Println("Request: ", req.RequestContext.HTTP.Method, req.RequestContext.HTTP.Path, req.RawPath, req.Headers, req.QueryStringParameters, req.RawQueryString)
 
 	decodedBody := []byte(req.Body)
 	if req.IsBase64Encoded {
@@ -167,6 +167,8 @@ func (r *RequestAccessorV2) EventToRequest(req events.APIGatewayV2HTTPRequest) (
 		log.Println(err)
 		return nil, err
 	}
+
+	fmt.Println("HTTP Request :", httpRequest, err)
 
 	httpRequest.RemoteAddr = req.RequestContext.HTTP.SourceIP
 
